7f18056a5ceabcf3c990cd182e9f9806bcbd34be
0327-07 <utchaynwaogu@gmail.com>
2022-07-12 11:34:01 +0200

    footer and navbar

---
+++ client/src/components/JuiceLab.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styledComponents from 'styled-components'
+
+
+
+
+const Container = styledComponents.div`
+height: 60vh;
+
+`
+
+const JuiceLab = () => {
+  return (
+    <div>
+    <Container>
+
+    </Container>
+    </div>
+  )
+}
+
+export default JuiceLab
