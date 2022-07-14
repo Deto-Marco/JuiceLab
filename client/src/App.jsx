e3d0a4eb04de15c965c42f1913dec3db68c135dc
0327-07 <utchaynwaogu@gmail.com>
2022-07-14 14:24:21 +0200

    juselab and routings are done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import Bulletin from './components/Bulletin';
 import Footer from './components/Footer';
 import JuiceLab from './components/JuiceLab';
 import Navbar from './components/Navbar';
+import ShoppingCart from './components/ShoppingCart';
 
 const App = () => {
   return (
     <main>
+      <ShoppingCart/>
     <Bulletin/>
     <Navbar/>
     <JuiceLab/>
--- client/src/components/JuiceLab.jsx
+++ client/src/components/JuiceLab.jsx
@@ -1,22 +1,119 @@
 import React from 'react'
 import styledComponents from 'styled-components'
+import {Link} from "react-router-dom"
 
-
-
+const DisplayMessage = styledComponents.div`
+display: flex;
+height: 10vh;
+align-items: center;
+justify-content: center;
+background-color: magenta;
+`
 
 const Container = styledComponents.div`
 height: 60vh;
+display: flex;
+`
+
+const Left = styledComponents.div`
+flex: 1;
+display: flex;
+flex-direction: column;
+padding: 20px;
 
 `
+
+
+const Center = styledComponents.div`
+flex: 1;
+padding: 20px;
+`
+
+
+const Right = styledComponents.div`
+flex: 1;
+padding: 20px;
+
+`
+const Summary = styledComponents.div`
+flex: 1;
+border: 0.5px solid lightgray;
+border-radius: 10px;
+padding: 20px;
+height: 35vh;
+
+`
+
+const SummaryTitle = styledComponents.h1`
+font-weight: 200;
+
+`
+
+const SummaryItem = styledComponents.div`
+margin: 30px 0px;
+display: flex;
+justify-content: space-between;
+font-weight: ${props=>props.type === "total" && "500"};
+font-size: ${props=>props.type === "total" && "24px"}; 
+`
+
+const SummaryText = styledComponents.span`
+
+`
+
+const SummaryPrice = styledComponents.span`
+
+`
+
+const SummaryButton = styledComponents.button`
+width: 100%;
+padding: 10px;
+background-color: red;
+color: white;
+font-weight: 600;
+`
+
 
 const JuiceLab = () => {
   return (
     <div>
+      <DisplayMessage>This is the welcome display message</DisplayMessage>
     <Container>
+    
+    <Left>This is a test</Left>
 
-    </Container>
-    </div>
-  )
+
+    <Center>This is a test</Center>
+    
+    
+    <Right>
+    <Summary>
+                    <SummaryTitle>Your Order</SummaryTitle>
+                    <SummaryItem>
+                        <SummaryText>Mango Splash</SummaryText>
+                        <SummaryPrice>€ 2.00</SummaryPrice>
+                    </SummaryItem>
+                    <SummaryItem>
+                        <SummaryText>Coconut Sunrise</SummaryText>
+                        <SummaryPrice>€ 2.00</SummaryPrice>
+                    </SummaryItem>
+                    <SummaryItem>
+                        <SummaryText>Summer Discount</SummaryText>
+                        <SummaryPrice>€ -0.00</SummaryPrice>
+                    </SummaryItem>
+                    <SummaryItem type="total">
+                        <SummaryText>Total</SummaryText>
+                        <SummaryPrice>€ 4.00</SummaryPrice>
+                    </SummaryItem>
+                    <Link to="/shoppingCart">
+                    <SummaryButton>Checkout Now</SummaryButton>
+                    </Link>
+                 
+                </Summary>
+    </Right>
+</Container>
+</div>
+)
 }
 
-export default JuiceLab
+export default JuiceLab 
